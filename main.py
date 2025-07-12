a9ac59f05a467cd9b51ffaeb4eba50ce2e03dd57
Louis Stevens <louis@louisstevens.com>
2025-07-13 00:26:08 +0100

    Adds Reflector class and the start of the letter tranformation function.

--- main.py
+++ main.py
@@ -22,6 +22,17 @@ class PlugboardConnection():
 
 	def getLettersInTuple(self) -> tuple:
 		return (self.letter0, self.letter1)
+	
+	@forceOnlyLetterStringsArgs(limitLengthToOne=True)
+	def getOppositeLetter(self, targetLetter: str) -> str:
+		if self.checkForLetter(targetLetter) == False:
+			raise ValueError(f"Failed to switch letter '{targetLetter}', not part of connection \'{self}\'")
+		
+		if targetLetter == self.letter0:
+			return self.letter1
+		else:
+			return self.letter0
+
 
 
 class Plugboard():
@@ -112,34 +123,78 @@ class Rotor():
 
 		turnoverTriggered = self.currentPosition == self.turnoverPosition
 		return turnoverTriggered
-	
 
 
-
+class Reflector():
+	# Right, so the more observant among you may
+	# notice that all of the Reflector class' code is
+	# shared with Rotor class. That means that Rotor
+	# Could have inheritted from Reflector instead of
+	# having this code twice. This change would take
+	# about a minute to make. But I haven't, because
+	# I don't feel like it. It just doesn't feel right.
+	def __init__(self) -> None:
+		self.name = name
+		self.outputMappingSequenceString = outputMappingSequenceString
 
 
 class EngimaMachine():
-	def __init__(self, plugboard: Plugboard, rotorList: list[str], reflector) -> None:
+	def __init__(self, plugboard: Plugboard, rotorList: list[Rotor], reflector) -> None:
 		self.plugboard = plugboard
 		self.rotorList = rotorList
 		self.reflector = reflector
 
 
+	def turnRotors(self) -> None:
+		if len(self.rotorList) == 0:
+			raise Exception('Failed to turn rotors as none are loaded into enigma machine.')
+		
+		currentRotorIndex = 0 
+		turnNextRotor = True
+
+		while turnNextRotor == True and currentRotorIndex < len(self.rotorList):
+			turnNextRotor = self.rotorList[currentRotorIndex].turnRotor()
+			currentRotorIndex += 1
+	
+
 	@forceOnlyLetterStringsArgs(limitLengthToOne=True)
-	def transformLetter(self, letter: str) -> str:
+	def processLetterInPlugboard(self, letterToProcess: str) -> str:
+		connectionWithLetter = self.plugboard.getConnectionByLetter(letterToProcess)
+		if connectionWithLetter != None:
+			return connectionWithLetter.getOppositeLetter(letterToProcess)
+		else:
+			return letterToProcess
+	
+	
+	@forceOnlyLetterStringsArgs(limitLengthToOne=True)
+	def processLetterInRotors(self, letterToSwitch: str, reverseOrder: bool=False) -> str:
 		pass
 
-	"""
-	- Turn rotors
-		- Turn rotor, check for turnover triger
-		- If there is another rotor loaded, set to current and repeat from last step
-	- Check switchboard for switch and perform if neccessary
-	- Send switchboard result through rotors (and reflector)
-		- Map letter via current rotor
-		- Check for next rotor, if found, repeat. If not, passthrough reflectors and repeat mappings IN REVERSE
-	- Check switchboard withj result and switch if neccessaary
-	- Return transformed letter
-	"""
+
+
+	@forceOnlyLetterStringsArgs(limitLengthToOne=True)
+	def transformLetter(self, letter: str) -> str:
+
+		self.turnRotors()
+		letterFromPlugboard = self.processLetterInPlugboard(letter)
+		letterFromRotors = ''
+
+
+
+
+		"""
+		- Turn rotors
+			- Turn rotor, check for turnover triger
+			- If there is another rotor loaded, set to current and repeat from last step
+		- Check switchboard for switch and perform if neccessary
+
+		- Send switchboard result through rotors (and reflector)
+			- Map letter via current rotor
+			- Check for next rotor, if found, repeat. If not, passthrough reflectors and repeat mappings IN REVERSE
+		
+		- Check switchboard withj result and switch if neccessaary
+		- Return transformed letter
+		"""
 
 
 	@forceOnlyLetterStringsArgs()
@@ -170,4 +225,23 @@ if __name__ == '__main__':
 
 	# print(plugboard)
 
+"""
+# GERMAN ARMY WW2 ROTORS
+Rotor # 	ABCDEFGHIJKLMNOPQRSTUVWXYZ		Date Introduced 	Model Name & Number
+I 			EKMFLGDQVZNTOWYHXUSPAIBRCJ		1930 				Enigma I
+II 			AJDKSIRUXBLHWTMCQGZNPYFVOE		1930 				Enigma I
+III 		BDFHJLCPRTXVZNYEIWGAKMUSQO		1930 				Enigma I
+IV 			ESOVPZJAYQUIRHXLNFTGKDCMWB		December 1938 		M3 Army
+V 			VZBRGITYUPSDNHLXAWMJQOFECK		December 1938 		M3 Army 
 
+
+TURNOVER NOTCH POSITIONS
+Rotor 		Notch 		Effect
+I 			Q 			If rotor steps from Q to R, the next rotor is advanced
+II 			E 			If rotor steps from E to F, the next rotor is advanced
+III 		V 			If rotor steps from V to W, the next rotor is advanced
+IV 			J 			If rotor steps from J to K, the next rotor is advanced
+V 			Z 			If rotor steps from Z to A, the next rotor is advanced 
+
+Note: Turnover position numbers should be the number for the letter AFTER the one in the notch column.
+"""
